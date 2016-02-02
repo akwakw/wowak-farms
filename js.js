87668aadfbd8b05c68e1e7eb6e54b29b6f65561a
Amy <akwowak@gmail.com>
2016-02-01 22:19:24 -0500

    first commit

---
+++ js.js
@@ -0,0 +1,23 @@
+// $(window).load(function() {    
+
+//   var theWindow        = $(window),
+//       $logo              = $("#one"),
+//       aspectRatio      = $one.width() / $one.height();
+                    
+//   function resizeLogo() {
+    
+//     if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
+//         $one
+//           .removeClass()
+//           .addClass('oneheight');
+//     } else {
+//         $one
+//           .removeClass()
+//           .addClass('onewidth');
+//     }
+          
+//   }
+                          
+//   theWindow.resize(resizeBg).trigger("resize");
+
+// });
